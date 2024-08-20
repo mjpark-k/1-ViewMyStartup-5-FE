3afa5322e0a0fb5cbda6503f1b605ac996a190ef
mjpark <pmj9498@gmail.com>
2024-08-20 11:15:23 +0900

    feat: AddCompany from MyCompany(중간)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
+import MyCompany from "./pages/MyCompany/MyCompany";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
+      <Routes>
+        <Route path="/myCompany" element={<MyCompany />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/MyCompany/MyCompany.js
@@ -0,0 +1,11 @@
+import AddCompany from "../../components/AddCompany/AddCompany";
+
+function MyCompany() {
+  return (
+    <>
+      <AddCompany />
+    </>
+  )
+}
+
+export default MyCompany;
