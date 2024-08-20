0770f3637dfd184bd7e12f4475505a7d11092e95
minsu <a2238737@gmail.com>
2024-08-20 15:44:21 +0900

    Feat : 투자현황 페이지

---
+++ src/components/ComparisonViewer/ComparisonViewer.js
@@ -0,0 +1,14 @@
+import "./ComparisonViewer.css";
+function ComparisonViewer() {
+  return (
+    <>
+      <div id="CompoersionViewer">
+        <div id="CompoersionViewer-container">
+          <p id="CompoersionViewer-title">비교 결과 확인하기</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ComparisonViewer;
