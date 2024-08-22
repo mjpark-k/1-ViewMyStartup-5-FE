5910f28a77909dd4f812a9edcd624bfa31e3f370
Minsugar98 <118067539+Minsugar98@users.noreply.github.com>
2024-08-22 11:43:57 +0900

    fix : Test 컴포넌트 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
-import Test from "./pages/test.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Nav />
-      <Test />
     </BrowserRouter>
   );
 }
