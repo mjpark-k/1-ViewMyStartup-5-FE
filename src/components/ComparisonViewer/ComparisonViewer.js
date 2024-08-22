6755ed6b5120273884b69d7b78b935c9c5c95cf5
minsu <a2238737@gmail.com>
2024-08-22 10:08:29 +0900

    Feat : 나의 기업 비교 결과(비교 결과 확인하기)

--- src/components/ComparisonViewer/ComparisonViewer.js
+++ src/components/ComparisonViewer/ComparisonViewer.js
@@ -1,11 +1,58 @@
 import "./ComparisonViewer.css";
+import ComparisonInvestList from "./InvestList/ComparisonInvestList.js";
+import ComparisonInvestTitle from "./InvestList/ComparisonInvestTitle.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function ComparisonViewer() {
+  const [ComparisonData, setComparisonData] = useState([]);
+  const [ComparisonOption, setComparisonOption] =
+    useState("누적 투자금액 낮은순");
+  const [ComparisonorderBy, setComparisonOrderBy] = useState("");
+  const [ComparisonsortOrder, setComparisonSortOrder] = useState("");
+  // const keywrods = ["엘리스", "코드잇"];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://startup-38qa.onrender.com/startups",
+        {
+          params: {
+            // keyword: keywrods,
+            limit: 5, // 나중에 리스트 길이만큼 정해주면 끝
+            sortBy: ComparisonorderBy,
+            sortOrder: ComparisonsortOrder,
+          },
+        }
+      );
+      // console.log(response.data.data);
+      setComparisonData(response.data.data);
+    };
+    fetchData();
+  }, [ComparisonorderBy, ComparisonsortOrder, ComparisonOption]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     console.log(oderBy);
+  //     console.log(sortOder);
+  //   };
+  //   fetchData();
+  // }, [oderBy, sortOder]);
+
   return (
     <>
       <div id="CompoersionViewer">
-        <div id="CompoersionViewer-container">
-          <p id="CompoersionViewer-title">비교 결과 확인하기</p>
-        </div>
+        <ComparisonInvestTitle
+          selectedOption={ComparisonOption}
+          setSelectedOption={setComparisonOption}
+          sortOrder={ComparisonsortOrder}
+          setSortOrder={setComparisonSortOrder}
+          orderBy={ComparisonorderBy}
+          setOrderBy={setComparisonOrderBy}
+        />
+        <ComparisonInvestList
+          startupData={ComparisonData}
+          setstartupData={setComparisonData}
+        />
       </div>
     </>
   );
---
+++ src/components/ComparisonViewer/InvestList/ComparisonInvestList.js
@@ -0,0 +1,32 @@
+import groupImage from "../../../images/logo-img.svg";
+import styles from "./InvestList.module.css";
+
+function InvestList({ startupData }) {
+  return (
+    <div className={styles.inner}>
+      <ul className={styles.th}>
+        <li>기업 명</li>
+        <li>기업 소개</li>
+        <li>카테고리</li>
+        <li>투자 금액</li>
+        <li>매출액</li>
+        <li>고용 인원</li>
+      </ul>
+      {startupData.map((startup, index) => (
+        <ul key={index} className={styles.list}>
+          <li>
+            <img src={groupImage} alt="그룹 이미지" />
+            {startup.name}
+          </li>
+          <li>{startup.description}</li>
+          <li>{startup.category}</li>
+          <li>{startup.simInvest}억</li>
+          <li>{startup.revenue}억</li>
+          <li>{startup.employees}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+export default InvestList;
