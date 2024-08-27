db8fdcad26ff40b8ec95ebe099656e0856e04d24
mini-1018 <anjaemin1018@gmail.com>
2024-08-27 13:20:19 +0900

    fix : 페이지네이션 버튼 api 추가

--- src/api.js
+++ src/api.js
@@ -1,35 +1,44 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://startup-38qa.onrender.com",
+  baseURL: 'https://startup-38qa.onrender.com',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
-export async function getCompany() {
-  const company = await api.get("/startups");
-  return company.data.data[0];
+export async function getCompany(params) {
+  const { id } = params;
+  const company = await api.get(`/startups/${id}`);
+  return company.data;
 }
 
-export async function getInvesterLength() {
-  const invester = await api.get(
-    `/startups/05e004c3-04a5-4b44-8f09-b0a55d51df0a/users`
-  );
+export async function getCompanyLength() {
+  const company = await api.get(`/startups`);
+  return company.data.meta.total;
+}
+
+export async function getAllDataLength(params) {
+  const query = `?keyword=${params}`;
+  const company = await api.get(`/startups${query}`);
+  return company.data.alldata;
+}
+
+export async function getInvesterLength(params) {
+  const { id } = params;
+  const invester = await api.get(`/startups/${id}/users`);
   return invester.data.users.length;
 }
 
 export async function getInvester(params) {
-  const query = new URLSearchParams(params).toString();
-  const invester = await api.get(
-    `/startups/05e004c3-04a5-4b44-8f09-b0a55d51df0a/users?${query}`
-  );
+  const { id, ...otherParams } = params;
+  const query = new URLSearchParams(otherParams).toString();
+  const invester = await api.get(`/startups/${id}/users?${query}`);
   return invester.data.rankUserData;
 }
 
-export async function deleteInvester(id) {
+export async function deleteInvester(params) {
+  const id = params;
   const response = await api.delete(`/users/${id}`);
   return response;
 }
-
-// 0a4788aa-b114-41e7-93b2-6e2e90367134
